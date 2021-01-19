f20c1a7aed08753ddbcf7bfa2ebd62b5b67abe6a
Fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2021-01-19 17:40:06 +0700

    menambah file bootstrap dan custom css,js

---
+++ asset/custome/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+   });
+
+   $('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    $(this).toggleClass('active');
+});
+    $(document).ready(function(){
+        $('ul li').click(function(){
+            $('li').removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
